05d80e959243fc6a40a37a47af222448c11002b7
Adesh Singh Rajawat <aadeshsingh1947@gmail.com>
2022-10-11 00:48:05 +0530

    some errors again

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,6 @@
 const userModel= require("../models/userModel")
 const jwt= require("jsonwebtoken")
+const { uploadFile } = require("../controller/aws")
 
 
 
@@ -9,6 +10,12 @@ const createUser= async function(req,res){
     try{
        
         let comingData= req.body
+         let profileImage=req.files
+        const uploadedImage = await uploadFile(profileImage[0])
+        comingData.profileImage = uploadedImage
+
+
+
         
         let savedData = await userModel.create(comingData)
        return res.status(201).send({status:true,data:savedData})
--- src/validation/validator.js
+++ src/validation/validator.js
@@ -1,146 +1,203 @@
-const userModel= require("../models/userModel")
+const userModel = require("../models/userModel");
 
+const validUser = async function (req, res, next) {
+  try {
+    let data = req.body;
 
+    let { fname, lname, email, profileImage, phone, password, address } = data;
 
-const validUser= async function(req,res,next){
-    try{
-     
-        let data = req.body
-       
-     let {fname,lname,email,profileImage,phone,password,address}= data
+    if (Object.keys(data) == 0)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input some data" });
 
-        if(Object.keys(data)==0)
-        return res.status(400).send({status:false, msg:"please input some data"})
+    //=========================== fname ==================================================================================================================================
 
-//=========================== fname ==================================================================================================================================           
+    if (!fname)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input First name" });
 
-       if(!fname)
-       return res.status(400).send({status:false, msg:"please input First name"})
+    if (!/^[A-Za-z]{3,10}/.test(fname))
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "Please enter valid fname, and no numbers",
+        });
 
-        if (!/^[A-Za-z]{3,10}/.test(fname))
-        return res.status(400).send({ status: false, message: "Please enter valid fname, and no numbers" });
-  
     //   if (typeof fname === "string" && fname.trim().length == 0)
     //     return res.status(400).send({ status: false, message: "input valid fname no extra space is allowed" });
-  
-//=========================== lname ==================================================================================================================================
 
-        if(!lname)
-        return res.status(400).send({status:false, msg:"please input Last name"})
+    //=========================== lname ==================================================================================================================================
 
-        if (!/^[A-Za-z]{3,10}/.test(lname))
-        return res.status(400).send({ status: false, message: "Please enter valid lname, and no numbers" });
-  
-//=========================== E-mail ==================================================================================================================================
+    if (!lname)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input Last name" });
 
-        if(!email)
-        return res.status(400).send({status:false, msg:"please input E-mail"})
+    if (!/^[A-Za-z]{3,10}/.test(lname))
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message: "Please enter valid lname, and no numbers",
+        });
 
-        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))
-        return res.status(400).send({ status: false, message: "Invalid Email Id" });
-  
-      let emailId = req.body.email;
-  
-      let validEmail = await userModel.findOne({ email: emailId });
-      if (validEmail)
-        return res.status(400).send({ status: false, message: "E-mail already taken" });
+    //=========================== E-mail ==================================================================================================================================
 
-//=========================== profile-Image ==================================================================================================================================
+    if (!email)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input E-mail" });
 
-        if(!profileImage)
-        return res.status(400).send({status:false, msg:"please input profile_image"})
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))
+      return res
+        .status(400)
+        .send({ status: false, message: "Invalid Email Id" });
 
-//=========================== phone ==================================================================================================================================        
+    let emailId = req.body.email;
 
-      if(!phone)
-        return res.status(400).send({status:false, msg:"please input phone Number"})
+    let validEmail = await userModel.findOne({ email: emailId });
+    if (validEmail)
+      return res
+        .status(400)
+        .send({ status: false, message: "E-mail already taken" });
 
-      let number = req.body.phone;
-      
-      if (await userModel.findOne({ phone: number }))
-        return res.status(400).send({ status: false, message: "phone number is already taken" });
-  
-      //-------------------------> REGEX <---------------------------------------------------------------------------------------------------------
-  
-      if (!/^[6-9]\d{9}$/.test(data.phone))
-        return res.status(400).send({ status: false, message: "Wrong Mobile Number" });
-  
-//=========================== password ==================================================================================================================================
+    //=========================== profile-Image ==================================================================================================================================
 
-        if(!password)
-        return res.status(400).send({status:false, msg:"please input password"})
+    // if(!profileImage)
+    // return res.status(400).send({status:false, msg:"please input profile_image"})
+
+    //=========================== phone ==================================================================================================================================
+
+    if (!phone)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input phone Number" });
+
+    let number = req.body.phone;
+
+    if (await userModel.findOne({ phone: number }))
+      return res
+        .status(400)
+        .send({ status: false, message: "phone number is already taken" });
+
+    //-------------------------> REGEX <---------------------------------------------------------------------------------------------------------
+
+    if (!/^[6-9]\d{9}$/.test(data.phone))
+      return res
+        .status(400)
+        .send({ status: false, message: "Wrong Mobile Number" });
+
+    //=========================== password ==================================================================================================================================
+
+    if (!password)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input password" });
 
     if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,15}$/.test(password))
-    return res.status(400).send({ status: false, message: "Atleat 1 uppercase, 1 lowercase, 1 numberic value , 1 special character and Length should be between 8 t0 14 for password!!!" });
+      return res
+        .status(400)
+        .send({
+          status: false,
+          message:
+            "Atleat 1 uppercase, 1 lowercase, 1 numberic value , 1 special character and Length should be between 8 t0 14 for password!!!",
+        });
 
-//=========================== address ==================================================================================================================================
+    //=========================== address ==================================================================================================================================
 
+    if (!address)
+      return res
+        .status(400)
+        .send({ status: false, msg: "please input adress" });
 
-        if(!address)
-        return res.status(400).send({status:false, msg:"please input adress"})
-          
-         let {shipping,billing}=address
-      
-       // empty object value is truthy so is no key present in adress in taht case also its go into if
-       
-        if (shipping) {
+    let { shipping, billing } = address;
 
-            let {street,city,pincode}=shipping
+    // empty object value is truthy so is no key present in adress in taht case also its go into if
 
-          if (!street) {
-            return res.status(400).send({ status: false, message: "shipping Street address cannot be empty" })
-          }
-          if (!/^[A-Za-z0-9]{3,10}/.test(street))
-          return res.status(400).send({ status: false, message: " street address not valid LOL 😵" });
-    
-          if (!city) 
-            return res.status(400).send({ status: false, message: "shipping City cannot be empty" })
-          
-            if (!/^[A-Za-z]{3,10}/.test(city))
-            return res.status(400).send({ status: false, message: "enter valid city 🙂!!" });
-      
-          if (!pincode) {
-            return res.status(400).send({ status: false, message: "shipping Pincode cannot be empty" })
-          }
+    if (shipping) {
+      let { street, city, pincode } = shipping;
 
-          if(!/^[1-9][0-9]{5}$/.test(pincode))
-          return res.status(400).send({status:false, msg:"wrong pincode"})
-       
-       }
-       
-       if(billing){
+      if (!street) {
+        return res
+          .status(400)
+          .send({
+            status: false,
+            message: "shipping Street address cannot be empty",
+          });
+      }
+      if (!/^[A-Za-z0-9]{3,10}/.test(street))
+        return res
+          .status(400)
+          .send({ status: false, message: " street address not valid LOL 😵" });
 
-        let {street,city,pincode}=billing
+      if (!city)
+        return res
+          .status(400)
+          .send({ status: false, message: "shipping City cannot be empty" });
 
-        if (!street) {
-            return res.status(400).send({ status: false, message: "billing Street address cannot be empty" })
-          }
-         
-          if (!/^[A-Za-z0-9]{3,10}/.test(street))
-          return res.status(400).send({ status: false, message: " street address not valid Broh 😌" });
-    
-          if (!city) {
-            return res.status(400).send({ status: false, message: "billing City cannot be empty" })
-          }
-         
-          if (!/^[A-Za-z]{3,10}/.test(city))
-          return res.status(400).send({ status: false, message: "enter valid city Broh  😐!!" });
-    
-          if (!pincode) {
-            return res.status(400).send({ status: false, message: "billing Pincode cannot be empty" })
-          }
+      if (!/^[A-Za-z]{3,10}/.test(city))
+        return res
+          .status(400)
+          .send({ status: false, message: "enter valid city 🙂!!" });
 
-          if(!/^[1-9][0-9]{5}$/.test(pincode))
-          return res.status(400).send({status:false, msg:"wrong pincode"})
-       
-       }
+      if (!pincode) {
+        return res
+          .status(400)
+          .send({ status: false, message: "shipping Pincode cannot be empty" });
+      }
 
-     next()
+      if (!/^[1-9][0-9]{5}$/.test(pincode))
+        return res.status(400).send({ status: false, msg: "wrong pincode" });
     }
-    catch(err){
-        return res.status(500).send({status:false, msg:err.message})
+
+    if (billing) {
+      let { street, city, pincode } = billing;
+
+      if (!street) {
+        return res
+          .status(400)
+          .send({
+            status: false,
+            message: "billing Street address cannot be empty",
+          });
+      }
+
+      if (!/^[A-Za-z0-9]{3,10}/.test(street))
+        return res
+          .status(400)
+          .send({
+            status: false,
+            message: " street address not valid Broh 😌",
+          });
+
+      if (!city) {
+        return res
+          .status(400)
+          .send({ status: false, message: "billing City cannot be empty" });
+      }
+
+      if (!/^[A-Za-z]{3,10}/.test(city))
+        return res
+          .status(400)
+          .send({ status: false, message: "enter valid city Broh  😐!!" });
+
+      if (!pincode) {
+        return res
+          .status(400)
+          .send({ status: false, message: "billing Pincode cannot be empty" });
+      }
+
+      if (!/^[1-9][0-9]{5}$/.test(pincode))
+        return res.status(400).send({ status: false, msg: "wrong pincode" });
     }
-}
 
+    next();
+  } catch (err) {
+    return res.status(500).send({ status: false, msg: err.message });
+  }
+};
 
-module.exports={validUser}
+module.exports = { validUser };

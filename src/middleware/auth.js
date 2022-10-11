a935cb262783cc37bd1e9c3ce9a475da297590be
KhomeshG <khomesshgajbhiye@gmail.com>
2022-10-11 22:10:16 +0530

    Autherization and Authentication part Put API is Not Complete Yet

---
+++ src/middleware/auth.js
@@ -0,0 +1,56 @@
+const jwt = require("jsonwebtoken");
+
+//Authentication
+exports.authentication = async function (req, res, next) {
+  try {
+    let tokenCheck = req.rawHeaders[1].replace("Bearer ", "");
+    console.log(tokenCheck);
+    if (tokenCheck == "undefined") {
+      return res
+        .status(400)
+        .send({ status: false, msg: "Token is required in bearer" });
+    }
+    //Verifying
+    let token = jwt.verify(tokenCheck, "project-5", (err, decode) => {
+      if (err) {
+        let msg =
+          err.message == "jwt expired"
+            ? "Token is Expired !!! "
+            : "Token is Invalid !!!";
+
+        return res.status(401).send({ status: false, msg: msg });
+      }
+      console.log(decode);
+      req["decode"] = decode.userId;
+    });
+
+    next();
+  } catch (err) {
+    return res.status(500).send({
+      status: false,
+      msg: "Server Error  authentication!!!",
+      ErrMsg: err.message,
+    });
+  }
+};
+
+//Autherization
+
+exports.autherization = async function (req, res, next) {
+  try {
+    console.log(typeof req.decode.toString(), typeof req.params.userId);
+    if (req.params) {
+      if (req.params.userId == req.decode.toString()) {
+        next();
+      } else {
+        return res
+          .status(403)
+          .send({ status: false, msg: "not Autherized User!!!" });
+      }
+    }
+  } catch (err) {
+    return res
+      .status(500)
+      .send({ status: false, msg: "Server Error autherization !!!" });
+  }
+};
